7c19e2b7e37c7db00c09979b18aad4255d30d9c7
MARY <marymwihaki123@gmail.com>
2022-08-04 15:33:15 -0500

    got server to run on correct PORT

---
+++ server.js
@@ -0,0 +1,62 @@
+'use strict';
+console.log ('server is connected to js');
+//killall -9 node
+// REQUIRE
+const express=require('express');
+const cors = require('cors');
+require ('dotenv').config();
+//getting data from data file
+let data = require('./data/weather.json');
+// const{response} = require ('express');
+// Use
+const app = express();
+app.use (cors());
+// if port wont work in 3001(in env file) go to 3002
+const PORT = process.env.PORT || 3002;
+/// verifying server
+app.get ('/',(request,response)=>{
+
+  response.send ('server is working?');
+
+});
+
+
+
+// add weather route
+app.get('./weather', (request,response,next)=>{
+  try{
+    let searchQuery= request.query.searchQuery;
+    // user has to enter the city we have data for
+    console.log('searchQuery',searchQuery);
+    let lat = request.query.lat;
+    let lon= request.query.lon;
+    let temp = request.query.temp;
+    let cityData = data.find(city => city.city_name=== searchQuery);
+    let lonLoc = data.find(lonLoc=>lonLoc.lon=== lon);
+    let latLoc = data.find(latLoc=>latLoc.lat ===lat);
+    let tempLoc = data.find(tempLoc=>tempLoc.temp === temp);
+    response.send =(`The temperature in ${searchQuery} is ${tempLoc}`);
+    return(new Forecast(cityData,lonLoc,latLoc, tempLoc));
+    // if user doesn't select the predetermined city throw an error
+
+  } catch(error){
+    response.status(500).send ('invalid location');
+
+    next(error);
+  }
+});
+app.get ('*',(request,response)=>{
+  response.send ('Error that route was not found' );
+});
+
+
+class Forecast{
+  constructor (forecastObject){
+    console.log('forecastObject',forecastObject);
+    this.datetime = forecastObject.date;
+    this.description= forecastObject.description;
+  }
+
+}
+
+app.listen(PORT,()=>console.log (`server is running on ${PORT}`));
